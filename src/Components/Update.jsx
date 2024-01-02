99d61cff172db04d2d66bb9bfdbc9418f8c6874d
Andrea-vicari <andrea.vicari77@gmail.com>
2024-01-02 12:41:03 +0100

    Ok almost done Edit part

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import React from 'react'
 import { useEffect , useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import MP3_List from "./mp3.json";
 
 
 function Update (){
@@ -16,7 +17,7 @@ function Update (){
     const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get('http://localhost:3030/tracks/'+id)
+        axios.get('http://localhost:3000/tracks/'+id)
         .then(res => setInputData(res.data))
         .then(console.log(inputData))
         .catch(err => console.log(err))
@@ -24,30 +25,34 @@ function Update (){
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        axios.put('http://localhost:3030/tracks/'+id , inputData)
+        axios.put('http://localhost:3000/tracks/'+id , inputData)
         .then(res => {
             alert("Data Updated Successfully!")
-            navigate('/')
+            navigate('/Dashboard')
         })
     }
 
 
     return(
         <main className="container">
+                <h1 className="h3 mb-3 text-white">Edit: {id}</h1>
+                <p className="text-white">Choose the Track for this key</p>
                 <form onSubmit={handleSubmit}>
 
-                    <h1 className="h3 mb-3 text-white">Edit: {id}</h1>
-                    <p className="text-white">Choose the Track for this key</p>
+
                     <div className="form-floating">
 
 
-                    <select className="form-select mb-2" aria-label="Default select example">
+                    <select value={inputData} onChange={e => setInputData({...inputData, trackURL: e.target.value})} className="form-select mb-2" aria-label="Default select example">
+                    {MP3_List.tracks.map((e)=>{
+                  return (
+                        <option value={`${e.trackURL}`}>{e.trackURL}</option>
 
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
+                        );})}
                     </select>
+
                     </div>
-                    <button className="btn btn-primary w-100 py-2" type="submit">Confirm</button>
+                    <button className="btn btn-primary w-100 py-2" type="submit" onChange={e => setInputData({...inputData, trackURL: e.target.value})}>Confirm</button>
 
                 </form>
         </main>
